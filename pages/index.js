f40761ae4895e13e8b09aba2ebe4b2b2faec49f1
vicheanorm <vicheanorm@gmail.com>
2021-08-22 15:23:14 +0700

    add game lucky draw

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,22 @@
 import Head from 'next/head'
+import { useState } from 'react'
+import CountUp from 'react-countup';
+import ParticlesBg from 'particles-bg'
+
+
+
 
 export default function Home() {
+  
+  const [minVal, setMinVal]= useState(0);
+  const [maxVal, setMaxVal]= useState(0);
+  const [randomNum, setRandomNum] = useState(0);
+
+const handleRandomNum = () =>{
+  setRandomNum(Math.floor(Math.random()*(maxVal - minVal + 1) + maxVal));
+} 
+
+ 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
@@ -8,13 +24,32 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <div>
-        <h1 className="text-4xl text-red-400 font-semibold uppercase animate-bounce">Asurraa</h1>
-        <iframe width='700' height="700" src="https://embed.lottiefiles.com/animation/70385"/>
+        
+        
+        <img className="w-96" src="/watermark.png"/> 
         </div>
-      </main>
+        <div className="w-full h-full">
+          <h1> <span className="text-9xl text-gray-600 font-semibold">RWS-</span>
+          <CountUp className="text-9xl text-red-400 font-semibold" start={0} end={randomNum} duration={4} delay={20}></CountUp>
+          </h1>
+          {/* <p className="text-3xl text-red-400">RANDOM NUMBER : <span className="font-extrabold text-5xl text-gray-700">{randomNum}</span> </p> */}
 
+          <div className="flex justify-center py-5 items-center">
+          <p className="uppercase font-medium text-gray-500">Min Number: </p>
+          <input class="block w-80 mx-2 px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" type="number " value={minVal} onChange={e =>setMinVal(e.target.value)}/>
+          </div>
+          <div className="flex justify-center py-3 items-center">
+          <p className="uppercase font-medium text-gray-500">Max Number: </p>
+          <input class="block w-80 mx-2 px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" type="number " value={maxVal} onChange={e =>setMaxVal(e.target.value)}/>
+          </div>
+          <button className=" bg-red-400 text-white px-36 h-10 rounded-md mx-4" onClick={handleRandomNum}>Get RANDOM Number</button>
+        </div>
+        <ParticlesBg type="cobweb" bg={true}  />  
+      </main>
+      
       
     </div>
   )
